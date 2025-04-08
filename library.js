044630d8a918a95e95ebcc2dab2dbad71c8d9a18
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-04-08 14:59:06 +0100

    updated style, form works

--- library.js
+++ library.js
@@ -1,20 +1,19 @@
 const myLibrary = [];
 
-function Book(title, author) {
+function Book(title, author, pages) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
     this.title = title;
     this.author = author;
-    title = title.ToUpper();
+    this.pages = pages;
     const id = crypto.randomUUID();
-    this.bookDetails = `${title} by ${author} - ${id}`;
 };
 
-function addBookToLibrary(title, author) {
-    const newBook = new Book(title, author);
+function addBookToLibrary(title, author, pages) {
+    const newBook = new Book(title, author, pages);
     myLibrary.push(newBook);
-    displayLibraryBooks;
+    displayLibraryBooks();
 }
 
 function displayLibraryBooks() {
@@ -22,7 +21,19 @@ function displayLibraryBooks() {
         console.log(book.bookDetails);
         const divElement = document.createElement('div');
         divElement.setAttribute('class', 'library-book');
-        divElement.innerText = book.bookDetails;
+
+        const bookTitle = document.createElement('h4');
+        bookTitle.innerText = `Title: ${book.title}`;
+
+        const bookAuthor = document.createElement('p');
+        bookAuthor.innerText = `By ${book.author}`;
+
+        const bookPages = document.createElement('p');
+        bookPages.innerText = `Pages: ${book.bookPages}`;
+
+        divElement.appendChild(bookTitle);
+        divElement.appendChild(bookAuthor);
+        divElement.appendChild(bookPages);
 
         const libraryDisplay = document.querySelector('.library-display');
         libraryDisplay.appendChild(divElement);
@@ -30,7 +41,15 @@ function displayLibraryBooks() {
     });
 }
 
-console.log('test');
-addBookToLibrary('test title', 'test author');
-console.log(myLibrary);
-displayLibraryBooks();
+const form = document.getElementById('new-book-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    // const read = document.getElementById('been_read').checked;
+
+    addBookToLibrary(title, author, pages);
+    form.reset();
+});
