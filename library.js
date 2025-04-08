8f717b85447d7e7af1af495d4fa007edd3dc8a9e
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-04-08 15:30:43 +0100

    Added remove button to book

--- library.js
+++ library.js
@@ -1,44 +1,60 @@
-const myLibrary = [];
+let myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
     this.title = title;
     this.author = author;
     this.pages = pages;
-    const id = crypto.randomUUID();
+    this.read = read;
+    this.id = self.crypto.randomUUID();
 };
 
-function addBookToLibrary(title, author, pages) {
-    const newBook = new Book(title, author, pages);
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
     displayLibraryBooks();
 }
 
 function displayLibraryBooks() {
-    console.log(myLibrary);
     const libraryDisplay = document.querySelector('.library-display');
     libraryDisplay.innerHTML = '';
 
     myLibrary.forEach(book => {
-
+        const elementArray = [];
         console.log(book.bookDetails);
         const divElement = document.createElement('div');
         divElement.setAttribute('class', 'library-book');
+        divElement.setAttribute('id', book.id);
 
         const bookTitle = document.createElement('h4');
         bookTitle.innerText = `Title: ${book.title}`;
+        elementArray.push(bookTitle);
 
         const bookAuthor = document.createElement('p');
         bookAuthor.innerText = `By ${book.author}`;
+        elementArray.push(bookAuthor);
 
         const bookPages = document.createElement('p');
         bookPages.innerText = `Pages: ${book.bookPages}`;
+        elementArray.push(bookPages);
 
-        divElement.appendChild(bookTitle);
-        divElement.appendChild(bookAuthor);
-        divElement.appendChild(bookPages);
+        const bookRead = document.createElement('p');
+        bookRead.innerText = `Book has been read: ${book.read}`;
+        elementArray.push(bookRead);
+
+        const removeButton = document.createElement('button');
+        removeButton.setAttribute('id', `remove-button-${book.id}`);
+        removeButton.innerText = "Remove book?";
+        removeButton.addEventListener("click", () => {
+            removeBookFromLibrary(book.id);
+        });
+        elementArray.push(removeButton);
+
+        elementArray.forEach(element => {
+            divElement.appendChild(element);
+        })
 
         libraryDisplay.appendChild(divElement);
     });
@@ -51,8 +67,19 @@ form.addEventListener('submit', (e) => {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
-    // const read = document.getElementById('been_read').checked;
+    let read;
+    if (document.getElementById('yesRead').checked) {
+        read = 'yes'
+    } else {
+        read = 'no'
+    }
 
-    addBookToLibrary(title, author, pages);
+    addBookToLibrary(title, author, pages, read);
     form.reset();
 });
+
+function removeBookFromLibrary(id) {
+    myLibrary = myLibrary.filter((book) => book.id !== id);
+    displayLibraryBooks();
+}
+
